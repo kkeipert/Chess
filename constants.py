43f5201209fea0e3ce556dc70efbccc65b3a127b
kkeipert <91289867+kkeipert@users.noreply.github.com>
2022-02-05 19:28:35 +0100

    First Commit

---
+++ constants.py
@@ -0,0 +1,15 @@
+import pygame as py
+
+WIDTH, HEIGHT = 512, 512
+DIMENSION = 8
+SQUARE_SIZE = WIDTH//DIMENSION
+
+IMAGES = {}
+
+def load_images():
+    pieces = ["bR", "bN", "bB", "bQ", "bK", "bP", "wR", "wN", "wB", "wQ", "wK", "wP"]
+    
+    for pis in pieces:
+        IMAGES[pis] = py.transform.scale(py.image.load("images/" + pis + ".png"), (SQUARE_SIZE, SQUARE_SIZE))
+
+load_images()
